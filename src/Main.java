9dc21683f38d8cea0c93acb64e6ef2be04173c05
colindoucet <colincdoucet@gmail.com>
2026-01-12 13:13:25 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("hello world");
-        Thread.sleep(2000);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world");
+            Thread.sleep(2000);
+        }
+
 
 
 
